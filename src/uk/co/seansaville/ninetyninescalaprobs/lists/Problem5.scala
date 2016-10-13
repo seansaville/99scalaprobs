68934d107c4418154dfd2c78381781139e088644
Sean Saville <sean.saville@hotmail.co.uk>
2016-10-13 20:29:12 +0100

    Added solution to Problem 5

---
+++ src/uk/co/seansaville/ninetyninescalaprobs/lists/Problem5.scala
@@ -0,0 +1,16 @@
+package uk.co.seansaville.ninetyninescalaprobs.lists
+
+/**
+  * Problem 5: Reverse a list.
+  */
+class Problem5 {
+
+  def reverse[T](list: List[T]): List[T] = {
+    def reverseHelper[U](original: List[U], reversed: List[U]): List[U] = original match {
+      case Nil    => reversed
+      case h :: t => reverseHelper(t, h :: reversed)
+    }
+    reverseHelper(list, List[T]())
+  }
+
+}
