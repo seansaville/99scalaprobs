eddc995550f82700d21c1edc93f9864c6cbe7698
Sean Saville <sean.saville@hotmail.co.uk>
2016-10-14 23:36:11 +0100

    Added solution to Problem 8

---
+++ src/uk/co/seansaville/ninetyninescalaprobs/lists/Problem8.scala
@@ -0,0 +1,24 @@
+package uk.co.seansaville.ninetyninescalaprobs.lists
+
+/**
+  * Problem 8: Eliminate consecutive duplicates of list elements.
+  */
+class Problem8 {
+
+  def compress[T](list: List[T]): List[T] = {
+    def compressHelper(list: List[T], currentItem: T): List[T] = list match {
+      case h :: t =>
+        if (h == currentItem)
+          compressHelper(t, currentItem)
+        else
+          h :: compressHelper(t, h)
+      case _      => list
+    }
+
+    list match {
+      case h :: t => h :: compressHelper(list, h)
+      case _      => list
+    }
+  }
+
+}
