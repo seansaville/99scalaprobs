240c737e748d24b5c76049b3ba55c57a93e99719
Sean Saville <sean.saville@hotmail.co.uk>
2016-10-19 21:44:54 +0100

    Added solution to Problem 12

---
+++ src/uk/co/seansaville/ninetyninescalaprobs/lists/Problem12.scala
@@ -0,0 +1,10 @@
+package uk.co.seansaville.ninetyninescalaprobs.lists
+
+/**
+  * Problem 12: Decode a run-length encoded list.
+  */
+class Problem12 {
+
+  def decode[T](list: List[(Int, T)]): List[T] = list flatMap (pair => List.fill(pair._1)(pair._2))
+
+}
