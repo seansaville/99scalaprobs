8a6b73a08e88b3c0c4470e73a8f985711b5d0dc3
Sean Saville <sean.saville@hotmail.co.uk>
2016-10-24 21:04:16 +0100

    Added solution to Problem 14

---
+++ src/uk/co/seansaville/ninetyninescalaprobs/lists/Problem14.scala
@@ -0,0 +1,16 @@
+package uk.co.seansaville.ninetyninescalaprobs.lists
+
+/**
+  * Problem 14: Duplicate the elements of a list.
+  */
+class Problem14 {
+
+  def duplicate[T](list: List[T]): List[T] = {
+    def duplicateHelper[U](list: List[U], result: List[U]): List[U] = list match {
+      case h :: t => duplicateHelper(t, h :: h :: result)
+      case _      => result.reverse
+    }
+    duplicateHelper(list, List())
+  }
+
+}
