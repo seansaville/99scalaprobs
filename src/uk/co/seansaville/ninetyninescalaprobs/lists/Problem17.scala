faafde2f592c5b42fd2d5ca2a023daecf392897b
Sean Saville <sean.saville@hotmail.co.uk>
2016-10-26 21:32:28 +0100

    Added solution to Problem 17

---
+++ src/uk/co/seansaville/ninetyninescalaprobs/lists/Problem17.scala
@@ -0,0 +1,17 @@
+package uk.co.seansaville.ninetyninescalaprobs.lists
+
+/**
+  * Problem 17: Split a list into two parts.
+  */
+class Problem17 {
+
+  def split[T](n: Int, list: List[T]): (List[T], List[T]) = {
+    def splitHelper[U](n: Int, list: List[U], first: List[U]): (List[U], List[U]) = (n, list) match {
+      case (0, second) => (first.reverse, second)
+      case (k, h :: t) => splitHelper(k - 1, t, h :: first)
+      case (k, _)      => (first.reverse, List())
+    }
+    splitHelper(n, list, List())
+  }
+
+}
