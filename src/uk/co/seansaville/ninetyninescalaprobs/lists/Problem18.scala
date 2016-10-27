65774d5a2afbda143e202545b17bf4f9c32ba2fa
Sean Saville <sean.saville@hotmail.co.uk>
2016-10-27 23:31:23 +0100

    Added solution to Problem 18

---
+++ src/uk/co/seansaville/ninetyninescalaprobs/lists/Problem18.scala
@@ -0,0 +1,19 @@
+package uk.co.seansaville.ninetyninescalaprobs.lists
+
+/**
+  * Problem 18: Extract a slice from a list.
+  */
+class Problem18 {
+
+  def slice[T](i: Int, k: Int, list: List[T]): List[T] = {
+    def sliceHelper[U](a: Int, b: Int, ls: List[U], result: List[U]): List[U] = (a, b, ls) match {
+      case (0, 1, h :: t) => (h :: result).reverse
+      case (0, n, h :: t) => sliceHelper(0, n - 1, t, h :: result)
+      case (0, _, _)      => result.reverse
+      case (m, n, h :: t) => sliceHelper(m - 1, n - 1, t, result)
+      case (_, _, _)      => result.reverse
+    }
+    sliceHelper(i, k, list, List())
+  }
+
+}
