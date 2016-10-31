e917644b9e367963c2bb3f8744d1c437e3c75277
Sean Saville <sean.saville@hotmail.co.uk>
2016-10-31 22:10:33 +0000

    Added solution to Problem 19

---
+++ src/uk/co/seansaville/ninetyninescalaprobs/lists/Problem19.scala
@@ -0,0 +1,21 @@
+package uk.co.seansaville.ninetyninescalaprobs.lists
+
+/**
+  * Problem 19: Rotate a list N places to the left.
+  */
+class Problem19 {
+
+  def rotate[T](n: Int, list: List[T]): List[T] = {
+    def rotateHelper[U](n: Int, list: List[U], temp: List[U]): List[U] = (n, list) match {
+      case (0, ls)     => ls ++ temp.reverse
+      case (k, h :: t) => rotateHelper(k - 1, t, h :: temp)
+      case (k, Nil)    => rotateHelper(k - 1, temp, List())
+    }
+
+    if (n < 0)
+      rotateHelper(list.length + n, list, List())
+    else
+      rotateHelper(n, list, List())
+  }
+
+}
