4685b5073927d655df1e44477ceb9a4005917c13
Sean Saville <sean.saville@hotmail.co.uk>
2016-11-01 22:18:18 +0000

    Added solution to Problem 20

---
+++ src/uk/co/seansaville/ninetyninescalaprobs/lists/Problem20.scala
@@ -0,0 +1,17 @@
+package uk.co.seansaville.ninetyninescalaprobs.lists
+
+/**
+  * Problem 20: Remove the Kth element from a list.
+  */
+class Problem20 {
+
+  def removeAt[T](k: Int, list: List[T]): (List[T], T) = {
+    def removeHelper[U](k: Int, list: List[U], temp: List[U]): (List[U], U) = (k, list) match {
+      case (0, h :: t) => (temp.reverse ++ t, h)
+      case (n, h :: t) => removeHelper(n - 1, t, h :: temp)
+      case (_, Nil)    => throw new NoSuchElementException
+    }
+    removeHelper(k, list, List())
+  }
+
+}
