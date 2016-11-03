05a5be2a71aee610d5cb06516785367183901b19
Sean Saville <sean.saville@hotmail.co.uk>
2016-11-03 23:44:35 +0000

    Small spacing change

--- src/test/scala/uk/co/seansaville/ninetyninescalaprobs/lists/Problem1Spec.scala
+++ src/test/scala/uk/co/seansaville/ninetyninescalaprobs/lists/Problem1Spec.scala
@@ -6,7 +6,7 @@ class Problem1Spec extends UnitSpec {
 
   "last" should "return the last element of a list with at least one element" in {
     val singleElementList = List(1)
-    val multiElementList = List(1,2,3,4,5)
+    val multiElementList = List(1, 2, 3, 4, 5)
     assert(Problem1.last(singleElementList) == 1)
     assert(Problem1.last(multiElementList) == 5)
   }
